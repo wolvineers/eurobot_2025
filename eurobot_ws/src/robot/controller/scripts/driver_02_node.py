86eea18f17e32263155974949e5dc996088249bf
Aida Benítez <benitezbague.aida@gmail.com>
2025-03-14 03:33:51 +0100

    EUR-26 - Second driver node. Insomnious actuators started to implement

--- eurobot_ws/src/robot/controller/scripts/driver_02_node.py
+++ eurobot_ws/src/robot/controller/scripts/driver_02_node.py
@@ -34,6 +34,12 @@ class SecondDriverNode(Node):
         # Subscribers
         self.action_commands_sub_  = self.create_subscription(JointTrajectory, '/controller/action_commands', self.actions_commands_callback, 10)
 
+        # Publishers
+        self.end_motor_action_pub_  = self.create_publisher(Bool, '/controller/motor_action', 10)
+
+        # Timers
+        self.motor_action_tim_ = self.create_timer(self.timer_period_, self.motor_timer)
+
 
     def actions_commands_callback(self, action_commands):
         """
@@ -51,11 +57,46 @@ class SecondDriverNode(Node):
 
         for i in range(len(action_commands.joint_names)):
             # Get servo number and position
-            num_servo = action_commands.joint_names[i]
-            pos_servo = action_commands.points[0].positions[i]
+            num_actuator = action_commands.joint_names[i]
+
+            if action_commands.points[0].positions:
+                val_actuator = action_commands.points[0].positions[i]
+            else:
+                val_actuator = action_commands.points[0].velocities[i]
 
             # Send message to the ESP32
-            send_message(self.serial_port, f"{num_servo},{pos_servo}")
+            send_message(self.serial_port, f"{num_actuator},{val_actuator}")
+
+    def motor_timer(self):
+        """
+        Reads the message sended by Esp32 containing the encoders value and publish this data.
+        """
+        
+        # self.get_logger().info("Volta")
+        motor_msg = read_message(self.serial_port)
+        
+        end_msg = Bool()
+        end_msg.data = True
+        self.end_motor_action_pub_.publish(end_msg)
+
+        # self.get_logger().info(str(motor_msg))
+        
+        # if motor_msg != None:
+        #     self.get_logger().info("Missatge no nul")
+        #     # Separates the message into the three parts marked by commas and saves the values
+        #     message_parts   = motor_msg.split(",")
+        #     actuator        = message_parts[0]
+        #     actuator_value  = int(message_parts[1])
+
+        #     # Prepare and publish the message
+        #     end_msg = Bool()
+
+        #     if actuator == "EA" and actuator_value == 0:
+        #         end_msg.data = False
+        #     elif actuator == "EA" and actuator_value == 1:
+        #         end_msg.data = True
+
+        #     self.end_motor_action_pub_.publish(end_msg)
 
 
 ## *************************
