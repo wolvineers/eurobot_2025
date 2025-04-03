4f806a06426f60a7bef2e3cb14e92b88f5470705
Aida Benítez <benitezbague.aida@gmail.com>
2025-04-03 16:39:27 +0200

    EUR-14 - Aruco detection. Files structure created

---
+++ eurobot_ws/src/computing_zone/aruco_detection/build/CMakeFiles/t_aruco_detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t_aruco_detection.dir/src/aruco_detection.cpp.o"
+  "CMakeFiles/t_aruco_detection.dir/src/aruco_detection.cpp.o.d"
+  "t_aruco_detection"
+  "t_aruco_detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/t_aruco_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ eurobot_ws/src/computing_zone/aruco_detection/src/aruco_detection.cpp
@@ -0,0 +1,75 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace cv;
+using namespace std;
+using namespace aruco;
+
+int main() {
+    
+    // -- READING THE IMAGE -- 
+
+    // Save the possible paths of the image
+    string path_docker = "/wolvi/src/computing_zone/aruco_detection/assets/playmat.mp4";
+
+    // Read the image
+    Mat playmat = imread(path_docker);
+
+    // Read the video
+    VideoCapture cap(path_docker);
+
+    // Throw error in case the image is empty
+    if (!cap.isOpened()) {
+        cerr << "Error: Unable to open the video!" << endl;
+        return -1;
+    }
+
+
+    // -- ARUCO DETECTION --
+
+    // Set the dictionary
+    Ptr<Dictionary> opencvDict = getPredefinedDictionary(DICT_4X4_100);
+
+    // Detect the arucos in every frame
+    Mat frame;
+
+    while (true) {
+        // Check if the video has ended
+        if (!cap.read(frame)) {
+            cerr << "End of video." << endl;
+            break;
+        }
+
+        // Declare result variables
+        vector<int> ids;
+        vector<vector<Point2f>> corners;
+
+        // Detect arucos
+        detectMarkers(frame, opencvDict, corners, ids);
+
+
+        // -- ARUCO DRAWING --
+
+        // Draw all the corners
+        if (!ids.empty()) {
+            drawDetectedMarkers(frame, corners, ids);
+            for (size_t i = 0; i < ids.size(); i++) {
+                // cout << "ID detected: " << ids[i] << " with vectors in the coordenates: " << endl;
+                // for (const auto& point : corners[i]) cout << point << endl;
+            }
+        } else {
+            cout << "Not arucos detected." << endl;
+        }
+
+        // Show image with the results
+        imshow("Arucos detector", frame);
+        if (waitKey(20) == 'q') break;
+    }
+
+    cap.release();
+    destroyAllWindows();
+    return 0;
+}
