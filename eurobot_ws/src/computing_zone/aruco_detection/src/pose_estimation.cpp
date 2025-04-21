a9d36cb05ab1dd3cd2977ae34ca0916feaeba568
Aida Benítez <benitezbague.aida@gmail.com>
2025-04-21 12:10:49 +0200

    EUR-14 - Aruco detection. Robot pose estimation implemented

--- eurobot_ws/src/computing_zone/aruco_detection/src/pose_estimation.cpp
+++ eurobot_ws/src/computing_zone/aruco_detection/src/pose_estimation.cpp
@@ -14,7 +14,7 @@ using namespace aruco;
 // FUNCTIONS
 // *************************
 
-Mat parseCameraMatrix(istream& in) {
+Mat parse_camera_matrix(istream& in) {
     /*
         Parses the camera matrix from the input stream and stores the values in a 3x3 matrix.
     
@@ -49,7 +49,6 @@ Mat parseCameraMatrix(istream& in) {
 
         // Read values separated by spaces, commas, or semicolons
         while (ss >> val) {
-            cout << "VAL: " << val << endl;
             mat.at<double>(row, col++) = val;
             if (ss.peek() == ',' || ss.peek() == ';') ss.ignore();
         }
@@ -60,7 +59,7 @@ Mat parseCameraMatrix(istream& in) {
     return mat;
 }
 
-Mat parseDistCoeffs(istream& in) {
+Mat parse_dist_coeffs(istream& in) {
     /*
         Parses the distortion coefficients from the input stream and stores the values in a 1x5 matrix.
     
@@ -72,7 +71,7 @@ Mat parseDistCoeffs(istream& in) {
     */
     
     // Create an initialize the matrix and variables
-    Mat distCoeffs(1, 5, CV_64F);
+    Mat dist_coeffs(1, 5, CV_64F);
     string line;
 
     // Read the stream line by line
@@ -94,13 +93,13 @@ Mat parseDistCoeffs(istream& in) {
 
             // Read each number and assign it to the matrix
             while (ss >> val) {
-                distCoeffs.at<double>(0, col++) = val;
+                dist_coeffs.at<double>(0, col++) = val;
                 if (ss.peek() == ',' || ss.peek() == ';') ss.ignore();
             }
         }
     }
 
-    return distCoeffs;
+    return dist_coeffs;
 }
 
 
@@ -120,20 +119,20 @@ int main() {
 
     // Read the values of the camera calibration
     ifstream mtx_file("/wolvi/src/computing_zone/aruco_detection/calibration_values/camera_matrix_output.txt");
-    Mat mtx = parseCameraMatrix(mtx_file);
+    Mat mtx = parse_camera_matrix(mtx_file);
     mtx_file.close();
 
     ifstream dits_file("/wolvi/src/computing_zone/aruco_detection/calibration_values/distortion_coefficients_output.txt");
-    Mat dist = parseDistCoeffs(dits_file);
+    Mat dist = parse_dist_coeffs(dits_file);
     dits_file.close();
 
     // Calculare the optimal camera matrix
-    Mat newCameraMatrix; Rect roi;
-    newCameraMatrix = getOptimalNewCameraMatrix(mtx, dist, Size(w, h), 1, Size(w, h), &roi);
+    Mat new_camera_matrix; Rect roi;
+    new_camera_matrix = getOptimalNewCameraMatrix(mtx, dist, Size(w, h), 1, Size(w, h), &roi);
 
     // Undistort the image
     Mat dst;
-    undistort(img, dst, mtx, dist, newCameraMatrix);
+    undistort(img, dst, mtx, dist, new_camera_matrix);
 
     // Crop the image
     dst = dst(roi);
@@ -147,7 +146,7 @@ int main() {
     // -- ARUCO DETECTION --
 
     // Set the dictionary
-    Ptr<Dictionary> opencvDict = getPredefinedDictionary(DICT_4X4_100);
+    Ptr<Dictionary> opencv_dict = getPredefinedDictionary(DICT_4X4_100);
 
     // Declare result variables
     vector<int> ids;
@@ -155,83 +154,119 @@ int main() {
 
     // Convert image to gray to make the detection easier
     Mat gray;
-    cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    cvtColor(img, gray, COLOR_BGR2GRAY);
     equalizeHist(gray, gray);
 
     // Detect arucos
-    detectMarkers(gray, opencvDict, corners, ids);
-
-
-    // -- ARUCO DRAWING --
-
-    std::map<int, cv::Point2f> markerCenters;
+    detectMarkers(gray, opencv_dict, corners, ids);
 
     // Calculate the corners center
+    std::map<int, cv::Point2f> marker_centers;
+
     for (size_t i = 0; i < ids.size(); i++) {
         Point2f center(0, 0);
         for (const auto& point : corners[i]) { center += point; }  // Sum all the corners
         center *= 0.25f;  // Corners average
 
         // Store the center with its ID
-        markerCenters[ids[i]] = center;
+        marker_centers[ids[i]] = center;
 
         cout << "ID: " << ids[i] << " - Center: " << center << endl;
     }
 
 
-    // -- 3D POSE TRANSFORMATION --
+    // -- CAMERA POSE TRANSFORMATION --
 
     // Store the world points
-    map<int, cv::Point3f> boardPointsMM = {
-        {20, {2400, 600, 0}},    // real point: {2400, 600, 0}
-        {21, {600, 600, 0}},     // real point: {600, 600, 0}
-        {22, {2400, 1400, 0}},   // real point: {2400, 1400, 0}
-        {23, {600, 1400, 0}}     // real point: {600, 1400, 0}
+    map<int, cv::Point3f> board_points_mm = {
+        {20, {2400, 600, 0}},
+        {21, {600, 600, 0}},
+        {22, {2400, 1400, 0}},
+        {23, {600, 1400, 0}}
     };
 
     // Store the image points from the centers calculated before
-    set<int> referenceIDs = {20, 21, 22, 23};
+    set<int> reference_ids = {20, 21, 22, 23};
 
-    vector<cv::Point2f> imagePoints;
-    vector<cv::Point3f> boardPoints;
+    vector<cv::Point2f> image_points;
+    vector<cv::Point3f> board_points;
 
-    for (const auto& [id, center] : markerCenters) {
-        if (referenceIDs.count(id) && boardPointsMM.count(id)) {
-            imagePoints.push_back(center);
-            boardPoints.push_back(boardPointsMM[id]);
+    for (const auto& [id, center] : marker_centers) {
+        if (reference_ids.count(id) && board_points_mm.count(id)) {
+            image_points.push_back(center);
+            board_points.push_back(board_points_mm[id]);
         }
     }
 
-    Mat rvec, tvec;
-    solvePnP(boardPoints, imagePoints, mtx, dist, rvec, tvec);
+    // Compute the camera position
+    Mat board_rvec, board_tvec;
+    solvePnP(board_points, image_points, mtx, dist, board_rvec, board_tvec);
 
-    // Show the result
-    cout << "Rvec undistorted:\n" << rvec << endl;
-    cout << "Tvec undistorted:\n" << tvec << endl;
+    Mat board_rot;
+    Rodrigues(board_rvec, board_rot);
+    Mat board_rot_t = board_rot.t();
 
-    Mat R;
-    Rodrigues(rvec, R);
-    cout << "Rotation matrix R = \n" << R << std::endl;
+    Mat board_trans = (Mat_<double>(3,1) << board_tvec.at<double>(0), board_tvec.at<double>(1), board_tvec.at<double>(2));
+    Mat camera_pos = -board_rot_t * board_trans;
 
-    Mat Rt = R.t();
-    Mat t = (Mat_<double>(3,1) << tvec.at<double>(0), tvec.at<double>(1), tvec.at<double>(2) * 0.55);
-    Mat camPos = -Rt * t;
-    cout << "Camera position (playmat coordinates): \n" << camPos << endl;
+
+    // -- ROBOT POSE TRANSFORMATION --
+
+    // Read image where is the robot
+    Mat robot_img = imread("/wolvi/src/computing_zone/aruco_detection/assets/playmat_02/photo_5.png");
+
+    // Detect arucos codes  
+    vector<int> robot_ids;
+    vector<vector<Point2f>> robot_corners;
+
+    detectMarkers(robot_img, opencv_dict, robot_corners, robot_ids);
+
+    // Store the aruco points
+    float marker_length = 100.0;
+    vector<Point3f> robot_obj_points = {
+        {-marker_length/2,  marker_length/2, 0},
+        { marker_length/2,  marker_length/2, 0},
+        { marker_length/2, -marker_length/2, 0},
+        {-marker_length/2, -marker_length/2, 0}
+    };
+
+    set<int> reference_robot_ids = {6};
+
+    vector<cv::Point2f> image_points_robot;
+    for (size_t i = 0; i < robot_ids.size(); ++i) {
+        if (robot_ids[i] == 6) {
+            image_points_robot = robot_corners[i];
+            break;
+        }
+    }
+
+    // Compute the aruco position
+    Mat robot_rvec, robot_tvec;
+    solvePnP(robot_obj_points, image_points_robot, mtx, dist, robot_rvec, robot_tvec);
+    
+    Mat robot_rot;
+    Rodrigues(robot_rvec, robot_rot);
+
+    Mat robot_trans = (Mat_<double>(3,1) << robot_tvec.at<double>(0), robot_tvec.at<double>(1), robot_tvec.at<double>(2));
+    Mat p_board = board_rot_t * robot_trans + camera_pos;
+
+    double robot_x = p_board.at<double>(0);
+    double robot_y = p_board.at<double>(1);
+    double robot_z = p_board.at<double>(2);
+    cout << "Robot position (mm): X=" << robot_x << ", Y=" << robot_y << ", Z=" << robot_z << endl;
 
 
     // -- POINT PROJECTION --
     
-    vector<Point3f> realPoints = { Point3f(600, 1400, 0), Point3f(600, 600, 0), Point3f(2400, 1400, 0), Point3f(2400, 600, 0), Point3f(1500, 2000, 0), Point3f(1500, 0, 0) };
-    vector<Point2f> projectedPoints;
+    vector<Point3f> real_points = { Point3f(robot_x, robot_y, robot_z), Point3f(robot_x, robot_y, robot_z - 510) };
+    vector<Point2f> projected_points;
 
     // Projection
-    projectPoints(realPoints, rvec, tvec, mtx, dist, projectedPoints);
-
-    // Show projected point
-    cout << "Punt 3D " << realPoints[0] << " projectat a: " << projectedPoints[0] << endl;
+    projectPoints(real_points, board_rvec, board_tvec, mtx, dist, projected_points);
 
     // Draw projected points in the image
-    for (int i = 0; i < projectedPoints.size(); i++) { circle(img, projectedPoints[i], 5, Scalar(0, 0, 255), -1); }
+    circle(img, projected_points[0], 6, Scalar(0, 0, 0), -1);
+    circle(img, projected_points[1], 6, Scalar(0, 255, 0), -1);
 
     // Show the image result
     resize(img, img, Size(), 0.5, 0.5);
