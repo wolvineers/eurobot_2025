7317ae1690396c0ac362c05e4c97f1bbbd26fe12
Aida Benítez <benitezbague.aida@gmail.com>
2025-05-08 21:35:37 +0200

    EUR-34 - Implement gyro in movement. Driver 02 improved

--- esp32_utils/src/controller/esp32_02_safety.cpp
+++ esp32_utils/src/controller/esp32_02_safety.cpp
@@ -81,10 +81,6 @@
 //         sendMessage(imu_msg);
 //     }
 
-//     servos[0].write(50);
-//     servos[2].write(140);
-
-
 //     // === Read message ===
 
 //     std::string message = readMessage().c_str();
